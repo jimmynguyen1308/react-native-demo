7c41e011d7eb5145a7f7170ae2cb308460061af6
jimmynguyen1308 <jimmynguyen1308@gmail.com>
2023-03-29 21:37:08 +1000

    reset branch

--- types/stack.types.ts
+++
@@ -1,9 +0,0 @@
-type HomeStackParamList = undefined
-type ProfileStackParamList = {
-  name: string
-}
-
-export type RootStackParamList = {
-  Home: HomeStackParamList
-  Profile: ProfileStackParamList
-}
